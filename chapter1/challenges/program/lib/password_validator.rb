c8ba61ca6153537d0ec60b5b4bfc1f3cf85ac93d
Jody <Jhd1989@hotmail.co.uk>
2022-11-30 14:52:38 +0000

    Ready to be reviewed

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -2,5 +2,13 @@
 # - 8 or more characters
 # - include at least one of these characters: !, @, $, %, &
 
+
 def valid?(password)
+    x = password.length
+    if x > 7 && password.count('!@$%&') != 0
+        then return true 
+    else 
+        return false
+    end
 end
+
